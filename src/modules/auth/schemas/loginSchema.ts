88af5fa87335c3f9328f1078a1e4f5f83e6d75fd
EmerIlesS <emersonilesitp@gmail.com>
2026-05-06 13:02:53 -0500

    se configuro  la cabezera para las solicitudes de del frontend a la api gateway

--- src/modules/auth/schemas/loginSchema.ts
+++ src/modules/auth/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().email('Correo invalido'),
-  password: z.string().min(6, 'La contrasena debe tener al menos 6 caracteres'),
+  password: z.string().min(4, 'La contrasena debe tener al menos 6 caracteres'),
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>
